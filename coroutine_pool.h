fa433c91be5711998e225a148626c3ea410a969e
markshuang <895175589@qq.com>
2020-07-22 19:01:48 +0800

    Update coroutine_pool.h

--- coroutine_pool.h
+++ coroutine_pool.h
@@ -2,7 +2,7 @@
 #define __COROUTINE_POOL_H__
 
 static const int default_min_stack_size = 512 * 1024;
-static const int default_stack_size = 8 * 1024 * 1024;
+static const int default_stack_size = 8 *1024 * 1024;
 
 void InitCoWorkerPool(int min_stack_size = default_min_stack_size, int stack_size = default_stack_size);
 
