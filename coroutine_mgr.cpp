36ee8c380f510a014972116ff803f0388d1aba4c
markshuang <895175589@qq.com>
2020-07-31 14:21:51 +0800

    Add files via upload

--- coroutine_mgr.cpp
+++ coroutine_mgr.cpp
@@ -1,3 +1,155 @@
 #include "coroutine_mgr.h"
 
+#define CO_TASK_TIMER_SIZE 10000
 
+struct CoTask
+{
+	CoTask()
+	{
+		mCo = co_getcur();
+		mUserPtr = NULL;
+		mCallbackID = -1;
+	}
+
+	~CoTask() {}
+
+	stroutine *mCo;
+	void *mUserPtr;
+	int mCallbackID;
+
+	private:
+
+	CoTask(const CoTask &);
+	CoTask& operator = (CoTask &);
+};
+
+struct CoTaskTimerElemCmp
+{
+	bool operator()(const CoTaskTimerElem &l, const CoTaskTimerElem &r)
+	{
+		return l.GetTimeOut() > r.GetTimeOut();
+	}
+};
+
+void coroutine_sleep(CoTask *task, int ms)
+{
+	CoTaskTimerElem elem(task->mCallbackID, CoroutineAsyncMap::GetInstance().GetCurMs() + ms);
+	CoroutineAsyncMap::GetInstance().AddTimer(elem);
+	co_yield(task->mCo);
+}
+
+
+CoroutineAsyncMap::CoroutineAsyncMap()
+{
+	mCallbackID = 0;
+	mTaskTimerHeap.resize(CO_TASK_TIMER_SIZE);
+	mTaskTimerCnt = 0;
+}
+
+CoroutineAsyncMap::~CoroutineAsyncMap()
+{
+	for (auto iter = mCoTaskMap.begin(); iter != mCoTaskMap.end(); ++iter)
+	{
+		delete iter->second;
+	}
+	mCoTaskMap.clear();
+	mTaskTimerHeap.clear();
+}
+
+CoTask *CoroutineAsyncMap::Alloc()
+{
+	int callbackid = GetCallBackID();
+	if (callbackid < 0)
+	{
+		return NULL;
+	}
+
+	CoTask *task = new CoTask();
+	task->mCallbackID = callbackid;
+	mCoTaskMap[callbackid] = task;
+	return task;
+}
+
+void CoroutineAsyncMap::Free(CoTask *&task)
+{
+	mCoTaskMap.erase(task->mCallbackID);
+	delete task;
+	task = NULL;
+}
+
+CoTask *CoroutineAsyncMap::Find(int callbackid)
+{
+	auto iter = mCoTaskMap.find(callbackid);
+	if (mCoTaskMap.end() == iter)
+	{
+		return NULL;
+	}
+	return iter->second;
+}
+
+int CoroutineAsyncMap::GetCallBackID()
+{
+	if (mCallbackID < 1)
+	{
+		mCallbackID = 1;
+	}
+
+	auto iter = mCoTaskMap.find(mCallbackID);
+	int cnt = 0;
+	while (iter != mCoTaskMap.end())
+	{
+		++cnt;
+		if (cnt >= 100)
+		{
+			return -1;
+		}
+		++mCallbackID;
+		if (mCallbackID < 1)
+		{
+			mCallbackID = 1;
+		}
+		iter = mCoTaskMap.find(mCallbackID);
+	}
+	return mCallbackID++;
+}
+
+int CoroutineAsyncMap::Tick()
+{
+	mCurMs = coroutine_time_ms();
+
+	int cnt = 0;
+
+	while (mTaskTimerCnt > 0 && mTaskTimerHeap[0].GetTimeOut() <= mCurMs)
+	{
+		++cnt;
+		CoTaskTimerElem timer = mTaskTimerHeap[0];
+		std::pop_heap(&mTaskTimerHeap[0], &mTaskTimerHeap[mTaskTimerCnt], CoTaskTimerElemCmp());
+		--mTaskTimerCnt;
+
+		CoTask *task = Find(timer.mCallbackID);
+		if (NULL == task)
+		{
+			//already process
+		}
+		else
+		{
+			fprintf(stderr,"timeout %d, %lu\n", timer.mCallbackID, timer.GetTimeOut());
+			co_resume(task->mCo);
+		}
+	}
+	return cnt;
+}
+
+int CoroutineAsyncMap::AddTimer(const CoTaskTimerElem &elem)
+{
+	if (mTaskTimerCnt >= CO_TASK_TIMER_SIZE)
+	{
+		fprintf(stderr,"AddTimer error cnt %d\n", mTaskTimerCnt);
+		return -1;
+	}
+
+	mTaskTimerHeap[mTaskTimerCnt] = elem;
+	++mTaskTimerCnt;
+	std::push_heap(&mTaskTimerHeap[0], &mTaskTimerHeap[mTaskTimerCnt], CoTaskTimerElemCmp());
+	return mTaskTimerCnt;
+}
--- coroutine_mgr.h
+++ coroutine_mgr.h
@@ -1,6 +1,69 @@
 #ifndef __COROUTINE_MGR__
 #define __COROUTINE_MGR__
 
+#include <algorithm>
+#include <map>
+#include <vector>
+#include <stdint.h>
+#include <sys/time.h>
+#include "ctx_swap.h"
+
+static inline uint64_t coroutine_time_ms()
+{
+	struct timeval tv;
+	gettimeofday(&tv, NULL);
+	return (uint64_t)tv.tv_sec * 1000 + tv.tv_usec / 1000;
+}
+
+struct CoTask;
+
+void coroutine_sleep(CoTask *task, int ms);
+
+struct CoTaskTimerElem
+{
+	CoTaskTimerElem() : mCallbackID(-1), mTimeOut(0) {}
+	CoTaskTimerElem(int callbackid, uint64_t timeout_ms) : mCallbackID(callbackid), mTimeOut(timeout_ms) {}
+	uint64_t GetTimeOut() const
+	{
+		return mTimeOut;
+	}
+	int mCallbackID;
+	uint64_t mTimeOut;
+};
+
+class CoroutineAsyncMap
+{
+	public:
+		~CoroutineAsyncMap();
+		CoTask *Alloc();
+		void Free(CoTask *&task);
+		CoTask *Find(int callbackid);
+
+		int Tick();
+
+		static CoroutineAsyncMap &GetInstance()
+		{
+			static CoroutineAsyncMap ins;
+			return ins;
+		}
+
+		uint64_t GetCurMs()
+		{
+			return mCurMs;
+		}
+
+		int AddTimer(const CoTaskTimerElem &elem);
+
+	private:
+		CoroutineAsyncMap();
+		int GetCallBackID();
+
+		std::map<int32_t, CoTask *> mCoTaskMap;
+		int32_t mCallbackID;
+		uint64_t mCurMs;
+		std::vector<CoTaskTimerElem> mTaskTimerHeap;
+		uint32_t mTaskTimerCnt;
+};
 
 #endif
 
